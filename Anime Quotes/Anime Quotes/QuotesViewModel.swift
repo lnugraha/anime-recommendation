e88a62e3e930c501f296911cb17baddb066db98d
lnugraha <leo-nugraha@Leos-MacBook-Pro-M1-Max.local>
2022-06-11 13:24:51 +0800

    Generic API request function

--- Anime Quotes/Anime Quotes/QuotesViewModel.swift
+++ Anime Quotes/Anime Quotes/QuotesViewModel.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+
+final class QuotesViewModel: NSObject {
+
+    @Published public var quoteList: Array<QuotesModel> = [QuotesModel]()
+
+    public func sendApiRequest() {
+        
+    }
+    
+}
+
+extension URLSession {
+
+    enum CustomError: Error {
+        case invalidUrl
+        case invalidData
+    }
+
+    public func request<T: Codable>(url: URL?,
+                                    expecting: T.Type,
+                                    completionHandler: @escaping(Result<T, Error>)->Void) {
+        
+        guard let url = url else {
+            completionHandler(.failure(CustomError.invalidUrl))
+            return
+        }
+        
+        let task = dataTask(with: url) { data, _, error in
+
+            guard let data = data else {
+                if let error = error {
+                    completionHandler(.failure(error))
+                } else {
+                    completionHandler(.failure(CustomError.invalidData))
+                }
+                return
+            }
+
+            do {
+                let result = try JSONDecoder().decode(T.self, from: data)
+                completionHandler(.success(result))
+            } catch {
+                completionHandler(.failure(error))
+            }
+
+        }
+
+        task.resume()
+    }
+    
+    
+}
